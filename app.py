6714c08aa6cd04b9b0a2fafec3e5fdb2c1d9c274
trist <tristan.leblond@gmail.com>
2024-11-18 23:06:27 -0500

    .

--- app.py
+++ app.py
@@ -1,40 +1,56 @@
 from flask import Flask, request, jsonify, render_template
-from models import db
-#from models.stock import Stock
-
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine, MetaData, Table, select
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stocks.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+from models import db
 db.init_app(app)
+
+from models.action import Action, ActionPrix
+
+
 
 with app.app_context():
     db.create_all()
 
+engine = db.create_engine('sqlite:///stocks.db')
+conn = engine.connect()
 """
-# Ajouter, Lire, Mettre à jour et Supprimer des données de stocks
-@app.route('/stocks', methods=['POST'])
-def add_stock():
-    data = request.get_json()
-    nouvelle_stock = Stock(nom=data['nom'], prix_fermeture=data['prix_fermeture'], prix_maximum=data['prix_maximum'],
-                           prix_minimum=data['prix_minimum'], symbole="\image.png")
-    db.session.add(nouvelle_stock)
-    db.session.commit()
-    return jsonify({'message': 'Livre ajouté'}), 201
+Base = declarative_base()
+Base.metadata.create_all(engine)
+meta_data = db.MetaData()
+meta_data.reflect(bind=engine)
 """
 
+# Ajouter, Lire, Mettre à jour et Supprimer des données de stocks
+
+ACTIONS = conn.exec_driver_sql("select * from tablename")
+query = db.select(ACTIONS.c.Symbole)
+symboles = engine.execute(query).fetchall()
 
 @app.route("/menu_principal")
 def menu():
-    return render_template("menu_principal.html")
+    ACTIONS = meta_data.tables["action"]
+    query = db.select(ACTIONS.c.Symbole)
+    symboles = engine.execute(query).fetchall()
+    return render_template("menu_principal.html", symboles=symboles)
 
 
 @app.route("/ajouter", methods=['Post'])
 def ajouter():
     return render_template("ajouter.html")
 
-
+"""
+@app.route('/stocks', methods=['POST'])
+def ajouter_stock():
+    data = request.get_json()
+    new_stock = Action(nom_entreprise=data["nom"], symbole=data["symbole"], prix=data["prix"])
+    db.session.add(new_stock)
+    db.session.commit()
+    return jsonify({'message': 'Livre ajouté'}), 201 # CODE http 201 = pour "created"
+"""
 @app.route('/stocks', methods=['GET'])
 def get_stocks():
     pass
---
+++ models/action.py
@@ -0,0 +1,47 @@
+from sqlalchemy import UniqueConstraint
+from models import db
+
+
+class Action(db.Model):
+    symbole = db.Column(db.String(10), unique=True, primary_key=True, nullable=False)
+    nom_entreprise = db.Column(db.String(200), nullable=False)
+
+    def __init__(self, symbole, nom_entreprise):
+        self.symbole = symbole
+        self.nom = nom_entreprise
+
+    def __repr__(self):
+        return f"Action: {str(self.symbole)}, {self.nom} "
+
+
+class ActionPrix(db.Model):
+    symbole = db.Column(db.String(10), unique=True,  nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    prix = db.Column(db.Float, nullable=False)
+    prix_max = db.Column(db.Float, nullable=False)
+    prix_min = db.Column(db.Float, nullable=False)
+
+    __table_args__ = (UniqueConstraint('symbole', 'date', name='unique_symbole_date'),
+                      db.PrimaryKeyConstraint('symbole', 'date'))
+
+    def __init__(self, symbole, date, prix, prix_max, prix_min):
+        self.symbole = symbole
+        self.date = date
+        self.prix = prix
+        self.prix_max = prix_max
+        self.prix_min = prix_min
+
+
+
+    def __repr__(self):
+        return (f"Action: {str(self.symbole)}, {self.date} "
+                f"prix: {self.prix}, "
+                f"prix maximum: {self.prix_max}, "
+                f"prix minimum: {self.prix_min}")
+
+
+
+
+
+
+
