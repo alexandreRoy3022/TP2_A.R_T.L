c7234430c13dde0f5da565a00c66cc62b00db3e0
trist <tristan.leblond@gmail.com>
2024-11-25 16:50:05 -0500

    .

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify, render_template, redirect, url_for
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, MetaData, Table, select
 from sqlalchemy.orm import sessionmaker
 
@@ -20,37 +19,49 @@ engine = create_engine('sqlite:///instance///stocks.db', echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-@app.route("/", methods=["GET", "POST"])
+def obtenir_liste_symboles():
+    symboles = session.query(Action.symbole).all()
+    liste_symboles = [symbole[0] for symbole in symboles]
+    return liste_symboles
+
+@app.route("/", methods=["GET"])
 def menu():
-    if request.method == "GET":
-        symboles = session.query(Action.symbole).all()
-        liste_symboles = [symbole[0] for symbole in symboles]
-        return render_template("menu_principal.html", symboles=liste_symboles)
+        return render_template("menu_principal.html", symboles=obtenir_liste_symboles())
 
 @app.route("/operation_action", methods=["POST"])
 def operation_action():
     symbole = request.form['symbole']
-
+    date = request.form['date']
     operation = request.form['operation']
+
     if operation == "ajouter":
         return render_template("ajouter_action.html", symbole=symbole)
     elif operation == "modifier":
         action = session.query(Action).filter_by(symbole=symbole).first()
         return render_template("modifier_action.html", symbole=symbole, nom_entreprise = action.nom_entreprise)
-
+    elif operation == "supprimer":
+        action = session.query(Action).filter_by(symbole=symbole).first()
+        return render_template("supprimer_action.html", symbole=symbole, nom_entreprise = action.nom_entreprise)
+    elif operation == "ajouter_prix":
+        return render_template("ajouter_prix.html", symbole=symbole, date=date)
 
 @app.route("/ajouter_action", methods=['GET', 'POST'])
 def ajouter_action():
     if request.method == 'POST':
         symbole = request.form['symbole']
         nom_entreprise = request.form['nom_entreprise']
-        nouvelle_action = Action(symbole = symbole, nom_entreprise=nom_entreprise)
-        db.session.add(nouvelle_action)
-        db.session.commit()
-        return redirect(url_for('menu'))
+        action = session.query(Action).filter_by(symbole=symbole).first()
+        if action:
+            return render_template("menu_principal.html", symboles=obtenir_liste_symboles(), message="Cette action existe déja.")
+        else:
+            nouvelle_action = Action(symbole = symbole, nom_entreprise=nom_entreprise)
+            db.session.add(nouvelle_action)
+            db.session.commit()
+            return redirect(url_for('menu'))
     else:
         symbole = request.form.get('Symbole')
         return render_template("modifier_action.html", symbole = symbole)
+
 
 @app.route("/modifier_action", methods=['GET', 'POST'])
 def modifier_action():
@@ -69,6 +80,21 @@ def modifier_action():
         return render_template("modifier_action.html", symbole = symbole)
 
 
+@app.route("/supprimer_action", methods=['GET', 'POST'])
+def supprimer_action():
+    if request.method == 'POST':
+        symbole = request.form['symbole']
+        nom_entreprise = request.form['nom_entreprise']
+        action = session.query(Action).filter_by(symbole=symbole).first()
+        if action:
+            session.delete(action)
+            session.commit()
+            return redirect(url_for('menu'))
+        else:
+            return render_template("menu_principal.html", symboles=obtenir_liste_symboles(), message="Symbole innexistant")
+    else:
+        symbole = request.form.get('Symbole')
+        return render_template("supprimer_action.html", symbole = symbole)
 
 
 @app.route('/stocks', methods=['GET'])
